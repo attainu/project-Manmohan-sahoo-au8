cd7812a82fc060aee3d2d527f528bb4c5a991d34
Manmohan sahoo <manmohansahoo361@gmail.com>
2020-11-12 09:35:11 +0530

    create a dictionary

---
+++ project.py
@@ -0,0 +1,20 @@
+# junk file organiser
+
+# creating the dictionary
+
+DIRECTORIES = {
+  # images file extension
+  "IMAGES" : [".png",".img",".jpeg",".svg",".gif",".jpg"]
+  #videos file extension
+  "VIDEOS" : [".mp4",".webm",".3gp",".avi",".mov",".mpg"]
+  #audio file extension
+  "AUDIO" : [".mp3",".wma","m4a",".m4b",".wav",".aac",".flac"]
+  #document file extension
+  "DOCUMENTS" : [".txt",".docx",".doc",".ppt",".pdf",".pptx",".html5",".html",".htm",".xls","xlsx",""]
+  #archives file extension
+}
+
+
+
+
+
