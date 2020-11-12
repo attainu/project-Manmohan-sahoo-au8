fe9d99fe95ffe5a30b06121818857dbaa2865502
Manmohan sahoo <manmohansahoo361@gmail.com>
2020-11-12 09:39:00 +0530

    create dictionary

--- project.py
+++ project.py
@@ -1,18 +1,20 @@
 # junk file organiser
 
-# creating the dictionary
+# creating the dictionary and iside dictionary put all the file extension,that we have organize in separate folder
 
 DIRECTORIES = {
   # images file extension
-  "IMAGES" : [".png",".img",".jpeg",".svg",".gif",".jpg"]
+  "IMAGES" : [".png",".img",".jpeg",".svg",".gif",".jpg"],
   #videos file extension
-  "VIDEOS" : [".mp4",".webm",".3gp",".avi",".mov",".mpg"]
+  "VIDEOS" : [".mp4",".webm",".3gp",".avi",".mov",".mpg"],
   #audio file extension
-  "AUDIO" : [".mp3",".wma","m4a",".m4b",".wav",".aac",".flac"]
+  "AUDIO" : [".mp3",".wma","m4a",".m4b",".wav",".aac",".flac"],
   #document file extension
-  "DOCUMENTS" : [".txt",".docx",".doc",".ppt",".pdf",".pptx",".html5",".html",".htm",".xls","xlsx",""]
+  "DOCUMENTS" : [".txt",".docx",".doc",".ppt",".pdf",".pptx",".html5",".html",".htm",".xls","xlsx",""],
   #archives file extension
+  "ARCHIVES" : [".rar",".zip",".xar"]
 }
+
 
 
 
