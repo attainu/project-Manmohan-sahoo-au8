480959076b34e638a055bd894db8dc169beb77ba
Manmohan sahoo <manmohansahoo361@gmail.com>
2020-11-12 20:23:16 +0530

    organize by extension part complete

--- project.py
+++ project.py
@@ -1,20 +1,60 @@
 # junk file organiser
 
-# creating the dictionary and iside dictionary put all the file extension,that we have organize in separate folder
+# importinf modules from python library
 
-DIRECTORIES = {
-  # images file extension
-  "IMAGES" : [".png",".img",".jpeg",".svg",".gif",".jpg"],
-  #videos file extension
-  "VIDEOS" : [".mp4",".webm",".3gp",".avi",".mov",".mpg"],
-  #audio file extension
-  "AUDIO" : [".mp3",".wma","m4a",".m4b",".wav",".aac",".flac"],
-  #document file extension
-  "DOCUMENTS" : [".txt",".docx",".doc",".ppt",".pdf",".pptx",".html5",".html",".htm",".xls","xlsx",""],
-  #archives file extension
-  "ARCHIVES" : [".rar",".zip",".xar"]
-}
+import os
+import math
+import datetime
+import shutil
+# for command line
+import sys
+import argparse
 
+CURRENT_DIRECTORY=r"E:\folder"
+
+#organize by extension:
+
+def organizeByExtension(path):
+  files=os.listdir(path)
+  all_files=[]
+
+#split file extension
+  for i in files:
+    _, split = os.path.splitext(i)
+    if split not in all_files:
+      all_files.append(split)
+
+# creating the separate folders 
+  for extension in all_files:
+    if extension:
+      os.mkdir(os.path.join(path,extension))
+
+#moving all the files to their specific folder
+  for i in files:
+    _, extension = os.path.splitext(i)
+    old_path=os.path.join(path,i)
+    new_path=os.path.join(path,extension,i)
+    os.rename(old_path, new_path)
+
+
+
+#define the function for command line parsing
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--path",default=".",help="Which folder to organize ?")
+  parser.add_argument("--o",default="extension",help="organize by?",choices=["extension"])
+
+  args=parser.parse_args()
+  path=args.path
+  organize_by=args.o
+
+  if organize_by == "extension":
+    organizeByExtension(path)
+  else:
+    print("wrong input")
+
+if __name__ == "__main__":
+    main()
 
 
 
